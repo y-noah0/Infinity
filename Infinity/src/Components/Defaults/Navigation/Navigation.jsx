49878f16a67f9a495b8842d22f7096e1d52943e5
Yannick Noah <noah55009@gmail.com>
2024-08-16 17:46:10 +0200

    Added new responsive features to the homepage

--- Infinity/src/Components/Defaults/Navigation/Navigation.jsx
+++ Infinity/src/Components/Defaults/Navigation/Navigation.jsx
@@ -1,9 +1,33 @@
-// import style from './Navigation.module.css';
+import { useState } from 'react';
+import style from './Navigation.module.css';
+import logo from '../../../assets/Navbar/logo.png';
+
 function Navigation() {
-    return(
-        <div>
-            <h1>Navigation</h1>
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    return (
+        <div className={style.navbar}>
+            <img src={logo} alt="Logo" className={style.logo} />
+            <div className={style.hamburger} onClick={toggleNav}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav className={`${style.nav} ${isNavOpen ? style.open : ''}`}>
+                <ul className={style.navList}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+                <a href="#" className={style.btn_register}>Register</a>
+            </nav>
         </div>
-    )
+    );
 }
+
 export default Navigation;
--- Infinity/src/Components/Home/HeroSection/HeroSection.jsx
+++ Infinity/src/Components/Home/HeroSection/HeroSection.jsx
@@ -1,9 +1,30 @@
-// import style from './HeroSection.module.css';
-function HeroSection() {
-    return(
-        <div>
-            <h1>HeroSection</h1>
-        </div>
-    )
-}
+// HeroSection.js
+import styles from './HeroSection.module.css';
+import image from '../../../assets/Home/HeroSection/image.png'
+
+const HeroSection = () => {
+  return (
+    <div className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Server better with one faster flexible platform</h1>
+      <p className={styles.description}>
+        Our POS system software is designed to cater to various
+        industries, including bars, restaurants, coffee shops, pizzerias,
+        alimentation, salons, and more.
+      </p>
+      <div className={styles.buttonGroup}>
+        <button className={`${styles.button} ${styles.primaryButton}`}>Get a Demo</button>
+        <button className={`${styles.button} ${styles.secondaryButton}`}>Get Started</button>
+      </div>
+    </div>
+    <div className={styles.imageContainer}>
+      <div className={styles.circleBackground}></div>
+      <div className={styles.circleBackground}></div>
+      <div className={styles.circleBackground}></div>
+      <img src={image} alt="POS System" className={styles.image} />
+    </div>
+  </div>
+  );
+};
+
 export default HeroSection;
