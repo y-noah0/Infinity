cb576383b21d806f8fdfc630882b0ec629e66fe0
Yannick Noah <noah55009@gmail.com>
2024-08-19 23:24:46 +0200

    fixed featureone, added footer

--- Infinity/src/Components/Home/Features/FeatureTwo/FeatureTwo.jsx
+++ Infinity/src/Components/Home/Features/FeatureTwo/FeatureTwo.jsx
@@ -2,9 +2,9 @@ import style from './FeatureTwo.module.css'
 import devices_images from '../../../../assets/Home/HeroSection/image.png'
 function FeatureTwo() {
     return(
-        <div className={style.container}>
+        <div className={style.main_container}>
             <div className={style.left_section}>
-                <h1>Whether you’re starting small or going big, Toast is built for you</h1>
+                <h1>Whether you’re starting small or going <br />big, Toast is built for you</h1>
                 <img 
                 src={devices_images} 
                 alt="Devices showing the Toast app" 
@@ -17,12 +17,12 @@ function FeatureTwo() {
                     <p>Our POS system software is designed to cater to various industries, including bars,Our POS system software is designed</p>
                     <a href="#" className={style.learn_more}>Learn More</a>
                 </div>
-                <div className={style.card}>
+                <div className={style.card2}>
                     <h2>New Business Opening</h2>
                     <p>Our POS system software is designed to cater to various industries, including bars,Our POS system software is designed</p>
                     <a href="#" className={style.learn_more}>Learn More</a>
                 </div>
-                <div className={style.card}>
+                <div className={style.card3}>
                     <h2>New Business Opening</h2>
                     <p>Our POS system software is designed to cater to various industries, including bars,Our POS system software is designed</p>
                     <a href="#" className={style.learn_more}>Learn More</a>
